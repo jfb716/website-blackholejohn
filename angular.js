214541efa1831f083543533c420497327aee927c
John Black <john.f.black@icloud.com>
2016-09-17 22:30:32 -0400

    lots of updates, added tag, angular, jquery, fields, txt download

--- angular.js
+++ angular.js
@@ -6,3 +6,21 @@ myApp.directive("anTag", function(){
        templateUrl: 'anTag.html'
    } 
 });
+
+myApp.directive("anAd", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'anAd.html'
+   } 
+});
+
+
+myApp.controller('myCtrl', function($scope) {
+    $scope.placementid = "PLACEMENT_ID";
+    $scope.ctacolor = "rgb(108, 135, 216)";
+    $scope.fontfamily = "Arial, Helvetica, sans-serif";
+    $scope.subFontWeight = "800";
+    $scope.titFontWeight = "700";
+    $scope.ctaFontWeight = "700";
+    });
+
